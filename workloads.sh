894de2963e448dc805c4d7853610f5ece858d15d
Lucia Pons <lupones@inf.upv.es>
2018-07-09 11:11:32 +0200

    Remove - at the end of each line

--- workloads.sh
+++ workloads.sh
@@ -1,17 +1,30 @@
 # !/bin/bash
 
-inputdir=/home/lupones/XPL3/outputCSVfiles/workloadsGraphs/
+inputdir=/home/lupones/XPL3/outputCSVfiles/$1/workloads$1.yaml
+outputdir=/home/lupones/XPL3/outputCSVfiles/$1/workloads$2.out
+
+touch $outputdir
+
+while read f ; do 
+    echo $f
+    workload=$(echo $f | rev | cut -c 2- | rev)
+    workload=$(echo $workload | cut -c 4- )
+    echo $workload
+
+    IFS=',' read -ra ADDR <<< "$workload"
+    
+    count=1
+    for i in "${ADDR[@]}"; do
+        if [ $count -lt ${#ADDR[@]} ]
+        then
+            echo -n $i"-" >> $outputdir
+        else
+            echo -n $i >> $outputdir
+        fi
+        (( count++ ))        
+    done
+    echo "" >> $outputdir
+    
+done < $inputdir 
 
 
-DIR=$(echo $inputdir"*")
-
-touch workloads.out
-
-for f in $DIR
-do
-          if [[ $f == *Total* ]]
-          then
-                workload=$(echo $f | cut -d'_' -f 2)
-                echo $workload >>workloads.out
-          fi
-done
